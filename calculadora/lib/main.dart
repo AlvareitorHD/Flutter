e3ff7740dcda609f9169b89dfcbcb62334083d42
AlvareitorHD <alvareitor.14.03@gmail.com>
2024-04-12 01:01:25 +0200

    Hecha calculadora

--- calculadora/lib/main.dart
+++ calculadora/lib/main.dart
@@ -35,91 +35,365 @@ class CalculadoraScreen extends StatefulWidget {
 
 class _CalculadoraScreenState extends State<CalculadoraScreen> {
   String _operacion = '+';
-  double _n1 = 0, _n2 = 0, resultado = 0;
-  //final TextEditingController _controllerN1 = Controller();
-  final TextEditingController _controllerN2 = TextEditingController();
+  double _n1 = 0, _n2 = 0, resultado = 0, _resultado_anterior = 0;
+  String _impresion = '';
+
+  void aniadirNum(int n){
+    setState(() {
+      _impresion += n.toString();
+    });
+  }
+  void aniadirChar(String s){
+    setState(() {
+      _impresion += s;
+    });
+  }
+  void aniadirOp(String op){
+    setState(() {
+      _impresion += op;
+      _operacion = op;
+    });
+  }
+  void borrarTodo(){
+    setState(() {
+      _impresion = '';
+      _operacion = '+';
+      _n1 = 0;
+      _n2 = 0;
+      resultado = 0;
+    });
+
+  }
+
+  void borrar(){
+    String imp = '';
+    setState(() {
+      for(int i = 0; i < _impresion.length-1; i++){
+        imp += _impresion[i];
+      }
+      _impresion = imp;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     //Scaffold es la estructura visual de la App
     return Scaffold(
-      backgroundColor: Colors.black26,
+      backgroundColor: Colors.black87,
       // Cabecera de la App
       appBar: AppBar(
         title: Text(widget.title),
         titleTextStyle: TextStyle(color: Colors.red),
-        backgroundColor: Colors.black12,
+        backgroundColor: Colors.cyanAccent,
       ),
 //Cuerpo de la App
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
+      body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            DropdownButton<String>(value: _operacion,
-              onChanged: (newValue) {
-              setState(() {
-                _operacion = newValue.toString();
-              });
-            },
-          items: <String>['+','-','*','/','^'].map<DropdownMenuItem<String>>((String value){
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
+          children: [
+            Container(
+              child: Text(
+                '$_impresion',
+                style: TextStyle(
+                  fontSize: 50,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(color: Colors.cyanAccent, width: 1.0)), // Define una línea de borde inferior
+              ),
+
             ),
-            TextField(
-              //controller: _controllerN1,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Primer número'),
-              onChanged: (value){
-                setState(() {
-                  _n1 = double.parse(value);
-                });
-              },
+
+          SizedBox(height: 50),
+          Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            for (int i = 1; i <= 3; i++)
+              ElevatedButton(
+                  onPressed: (){aniadirNum(i);},
+                  child: Text(i.toString(),style: TextStyle(
+                    fontSize: 30, // Aumenté el tamaño de la fuente para resaltar el resultado
+                    fontWeight: FontWeight.bold, //negrita
+                    color: Colors.black, // Cambié el color del texto para resaltar el resultado positivo
+                  ),),
+                style: ElevatedButton.styleFrom(
+                minimumSize: Size(200, 100), // Ajusta el tamaño mínimo del botón
+            ),),
+            ElevatedButton(
+                onPressed: (){
+                  aniadirOp('+');
+                },
+                child: Text('+',style: TextStyle(
+                  fontSize: 30, // Aumenté el tamaño de la fuente para resaltar el resultado
+                  fontWeight: FontWeight.bold, //negrita
+                  color: Colors.black, // Cambié el color del texto para resaltar el resultado positivo
+                ),),
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(200, 100), // Ajusta el tamaño mínimo del botón
+              ),
             ),
-            TextField(
-              controller: _controllerN2,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Segundo número'),
-              onChanged: (value){
+          ],
+        ),
+        SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            for (int i = 4; i <= 6; i++)
+              ElevatedButton(
+                  onPressed: (){aniadirNum(i);},
+                  child: Text(i.toString(),style: TextStyle(
+                    fontSize: 30, // Aumenté el tamaño de la fuente para resaltar el resultado
+                    fontWeight: FontWeight.bold, //negrita
+                    color: Colors.black, // Cambié el color del texto para resaltar el resultado positivo
+                  ),),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(200, 100), // Ajusta el tamaño mínimo del botón
+                ),),
+            ElevatedButton(
+                onPressed: (){
+                  aniadirOp('-');
+                },
+                child: Text('-',style: TextStyle(
+                  fontSize: 30, // Aumenté el tamaño de la fuente para resaltar el resultado
+                  fontWeight: FontWeight.bold, //negrita
+                  color: Colors.black, // Cambié el color del texto para resaltar el resultado positivo
+                ),),
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(200, 100), // Ajusta el tamaño mínimo del botón
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            for (int i = 7; i <= 9; i++)
+              ElevatedButton(
+                  onPressed: (){
+                    aniadirNum(i);
+                    },
+                  child: Text(i.toString(),style: TextStyle(
+                    fontSize: 30, // Aumenté el tamaño de la fuente para resaltar el resultado
+                    fontWeight: FontWeight.bold, //negrita
+                    color: Colors.black, // Cambié el color del texto para resaltar el resultado positivo
+                  ),),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(200, 100), // Ajusta el tamaño mínimo del botón
+                ),
+              ),
+            ElevatedButton(
+                onPressed: (){
+                  aniadirOp('*');
+                },
+                child: Text('*',style: TextStyle(
+                  fontSize: 30, // Aumenté el tamaño de la fuente para resaltar el resultado
+                  fontWeight: FontWeight.bold, //negrita
+                  color: Colors.black, // Cambié el color del texto para resaltar el resultado positivo
+                ),),
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(200, 100), // Ajusta el tamaño mínimo del botón
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+                onPressed: (){
+                  aniadirChar('.');
+                },
+                child: Text('.',style: TextStyle(
+                  fontSize: 30, // Aumenté el tamaño de la fuente para resaltar el resultado
+                  fontWeight: FontWeight.bold, //negrita
+                  color: Colors.black, // Cambié el color del texto para resaltar el resultado positivo
+                ),),
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(200, 100), // Ajusta el tamaño mínimo del botón
+              ),
+            ),
+            ElevatedButton(
+              onPressed: (){
+                aniadirNum(0);
+                },
+            
+              child: Text('0',style: TextStyle(
+                fontSize: 30, // Aumenté el tamaño de la fuente para resaltar el resultado
+                fontWeight: FontWeight.bold, //negrita
+                color: Colors.black, // Cambié el color del texto para resaltar el resultado positivo
+              ),),
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(200, 100), // Ajusta el tamaño mínimo del botón
+              ),
+            ),
+            ElevatedButton(
+              onPressed: (){
+                Operacion operacion = Suma();
+                String n1 = '', n2 = '';
+                int i = 0;
+                // Extraer el primer número
+                if(_impresion[0]=='-'){
+                  n1 += _impresion[0];
+                  i++;
+                }
+                while (_impresion[i] != '+' && _impresion[i] != '-' && _impresion[i] != '*' && _impresion[i] != '/' && _impresion[i] != '^') {
+                  n1 += _impresion[i];
+                  i++;
+                }
+
+                // Extraer el operador
+                if(_impresion[i] == '+' || _impresion[i] == '-' || _impresion[i] == '*' || _impresion[i] == '/' || _impresion[i] == '^'){
+                  if(_impresion[i] != ''){
+                    _operacion = _impresion[i];
+                  }
+                }
+
+                // Extraer el segundo número
+                i++; // Pasar al siguiente carácter después del operador
+                while (i < _impresion.length) {
+                  n2 += _impresion[i];
+                  i++;
+                }
+                //print("Yo: $n1 $_operacion $n2");
+                if(n1 != ''){
+                  _n1 = double.parse(n1);
+                }else{
+                  _n1 = 0;
+                }
+                if(n2 != ''){
+                  _n2 = double.parse(n2);
+                } else{
+                  _n2 = 0;
+                }
+                switch(_operacion){
+                  case '+':
+                    operacion = Suma();
+                    break;
+                  case '-':
+                    operacion = Resta();
+                    break;
+                  case '*':
+                    operacion = Multiplicacion();
+                    break;
+                  case'/':
+                    operacion = Division();
+                    break;
+                  case '^':
+                    operacion = Potencia();
+                    break;
+                }
                 setState(() {
-                  _n2 = double.parse(value);
+
+                  resultado = operacion.calcular(_n1, _n2);
+                  _impresion = '';
+                  _n1 = 0;
+                  _n2 = 0;
+                  _resultado_anterior = resultado;
                 });
-              },
+              }, child: Text('=',style: TextStyle(
+              fontSize: 30, // Aumenté el tamaño de la fuente para resaltar el resultado
+              fontWeight: FontWeight.bold, //negrita
+              color: Colors.black, // Cambié el color del texto para resaltar el resultado positivo
+            ),),
+    style: ElevatedButton.styleFrom(
+    minimumSize: Size(200, 100), // Ajusta el tamaño mínimo del botón
+    ),
             ),
             ElevatedButton(
                 onPressed: (){
-                  Operacion operacion = Suma();
-                  switch(_operacion){
-                    case '+':
-                      operacion = Suma();
-                      break;
-                    case '-':
-                      operacion = Resta();
-                      break;
-                    case '*':
-                      operacion = Multiplicacion();
-                      break;
-                    case'/':
-                      operacion = Division();
-                      break;
-                    case '^':
-                      operacion = Potencia();
-                      break;
-                  }
-                  setState(() {
-                    resultado = operacion.calcular(_n1, _n2);
-                  });
-                }, child: Text('Calcular'),
+                  aniadirOp('/');
+                },
+                child: Text('/',style: TextStyle(
+                  fontSize: 30, // Aumenté el tamaño de la fuente para resaltar el resultado
+                  fontWeight: FontWeight.bold, //negrita
+                  color: Colors.black, // Cambié el color del texto para resaltar el resultado positivo
+                ),),
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(200, 100), // Ajusta el tamaño mínimo del botón
+              ),
             ),
+          ],
+        ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                  onPressed: (){
+                    borrarTodo();
+                  },
+                  child: Text('AC',style: TextStyle(
+                    fontSize: 30, // Aumenté el tamaño de la fuente para resaltar el resultado
+                    fontWeight: FontWeight.bold, //negrita
+                    color: Colors.black, // Cambié el color del texto para resaltar el resultado positivo
+                  ),),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(200, 100), // Ajusta el tamaño mínimo del botón
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: (){
+                    borrar();
+                  },
+                  child: Text('<',style: TextStyle(
+                    fontSize: 30, // Aumenté el tamaño de la fuente para resaltar el resultado
+                    fontWeight: FontWeight.bold, //negrita
+                    color: Colors.black, // Cambié el color del texto para resaltar el resultado positivo
+                  ),),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(200, 100), // Ajusta el tamaño mínimo del botón
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: (){
+                    setState(() {
+                      _impresion += _resultado_anterior.toString();
+                    });
+                  },
+                  child: Text('Ans',style: TextStyle(
+    fontSize: 30, // Aumenté el tamaño de la fuente para resaltar el resultado
+    fontWeight: FontWeight.bold, //negrita
+    color: Colors.black, // Cambié el color del texto para resaltar el resultado positivo
+    ),),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(200, 100), // Ajusta el tamaño mínimo del botón
+                ),
+              ),
+
+              ElevatedButton(
+                  onPressed: (){
+                    aniadirOp('^');
+                  },
+                  child: Text('^',
+                    style: TextStyle(
+                      fontSize: 30, // Aumenté el tamaño de la fuente para resaltar el resultado
+                      fontWeight: FontWeight.bold, //negrita
+                      color: Colors.black, // Cambié el color del texto para resaltar el resultado positivo
+                    ),
+                  ),
+    style: ElevatedButton.styleFrom(
+    minimumSize: Size(200, 100), // Ajusta el tamaño mínimo del botón
+    ),
+              ),
+
+            ],
+            ),
+            SizedBox(height: 40),
             Text(
-              'RESULTADO: $resultado',
-              style: TextStyle(fontSize: 20),
+              'Resultado: $resultado',
+              style: TextStyle(
+                fontSize: 50, // Aumenté el tamaño de la fuente para resaltar el resultado
+                fontWeight: FontWeight.bold, //negrita
+                color: Colors.green, // Cambié el color del texto para resaltar el resultado positivo
+              ),
             ),
+
           ],
         ),
       ),
